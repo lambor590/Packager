344010e608b2d5cb7d1cedc74f4a63e97b5dfce5
lambor590 <lambor590@gmail.com>
2024-01-02 01:41:50 +0100

    fix auto updates

--- electron/main/update.js
+++ electron/main/update.js
@@ -5,8 +5,11 @@ export function update() {
   autoUpdater.autoDownload = true
   autoUpdater.allowDowngrade = false
   autoUpdater.disableWebInstaller = true
-  
-  autoUpdater.on('update-downloaded', autoUpdater.quitAndInstall(true, true))
+  autoUpdater.autoInstallOnAppQuit = true
 
-  autoUpdater.checkForUpdatesAndNotify()
+  autoUpdater.on('update-downloaded', () => {
+    autoUpdater.quitAndInstall(true, true)
+  });
+
+  autoUpdater.checkForUpdates()
 }
