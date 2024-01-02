ed78488b128be687093e94a4500fe7d18621670e
lambor590 <lambor590@gmail.com>
2024-01-03 00:23:29 +0100

    add update notification to renderer process

--- electron/main/update.js
+++ electron/main/update.js
@@ -1,11 +1,20 @@
 import { autoUpdater } from 'electron-updater'
 
-export function update() {
+export function update(event) {
 
   autoUpdater.autoDownload = true
   autoUpdater.allowDowngrade = false
   autoUpdater.disableWebInstaller = true
   autoUpdater.autoInstallOnAppQuit = true
+  autoUpdater.forceDevUpdateConfig = true
+
+  autoUpdater.on('update-available', (_event) => {
+    event.sender.send('update-available', _event)
+  })
+
+  autoUpdater.on('download-progress', (_event, progressInfo) => {
+    event.sender.send('download-progress', _event, progressInfo)
+  })
 
   autoUpdater.on('update-downloaded', () => {
     autoUpdater.quitAndInstall(true, true)
