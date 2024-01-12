8c2a369415e6754ebcba603470bd326a77ac6339
lambor590 <lambor590@gmail.com>
2024-01-12 19:57:29 +0100

    remove unused code & add navbar to main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
-import { useEffect, useState } from 'react'
-import { ipcRenderer } from 'electron'
 import UpdateModal from './components/UpdateModal'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
-  const [appVersion, setAppVersion] = useState('')
-
-  useEffect(() => {
-    ipcRenderer.invoke('get-app-version').then(version => {
-      setAppVersion(version)
-    })
-  }, [])
 
   return (
     <>
-      <h1>Packager</h1>
-      <div>
-        <button className='btn btn-primary'>
-          Botón de prueba
-        </button>
-        <p>
-          Versión {appVersion}
-        </p>
-      </div>
+      <Navbar />
       <UpdateModal />
     </>
   )
