0abe976a969592e6fed6ea76d30b17c82172e168
lambor590 <lambor590@gmail.com>
2024-01-13 21:23:02 +0100

    refactor modal design

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,18 +1,20 @@
 import '../App.css'
 
-function Modal({ isOpen, title = null, children, canBeDismissed = true, onClose }) {
+function Modal({ isOpen, title = null, message, children = null, canBeDismissed = true, onClose }) {
   return (
     <dialog className={`modal backdrop-blur-md backdrop-brightness-90 ${isOpen ? 'modal-open' : ''}`}>
       <div className="modal-box text-left">
         <h3 className="font-bold text-lg">{title}</h3>
-        <p className="py-4">{children}</p>
-        <div className="modal-action">
-          <form method="dialog">
-            {canBeDismissed &&
+        <p className="pt-4">{message}</p>
+        {children}
+        {
+          canBeDismissed &&
+          <div className="modal-action">
+            <form method="dialog">
               <button className="btn border-none" onClick={onClose}>Cerrar</button>
-            }
-          </form>
-        </div>
+            </form>
+          </div>
+        }
       </div>
     </dialog>
   )
