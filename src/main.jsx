f97494bf1ac60536da5bd556992b8cc08bce617c
lambor590 <lambor590@gmail.com>
2024-01-21 23:26:33 +0100

    improve event remover on start & update version

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import App from './App.jsx'
 import { invoke } from '@tauri-apps/api/tauri'
 import './index.css'
 
-document.addEventListener('DOMContentLoaded', async () => {
+const openMainWindow = () => {
   ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   )
-  await invoke('close_splash')
-  document.removeEventListener('DOMContentLoaded', () => {})
-})
+  invoke('close_splash')
+  document.removeEventListener('DOMContentLoaded', openMainWindow)
+}
+
+document.addEventListener('DOMContentLoaded', openMainWindow)
